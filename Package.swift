e66c20b222fe293fcf059fa9bbd826d075a144d4
SreeKuttan_ <sreekuttan@techgentsia.com>
2023-07-09 14:42:05 +0530

    -- resource added

--- Package.swift
+++ Package.swift
@@ -23,7 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CountryDialCode",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Resources/CountryCodes.json")
+            ]),
         .testTarget(
             name: "CountryDialCodeTests",
             dependencies: ["CountryDialCode"]),
